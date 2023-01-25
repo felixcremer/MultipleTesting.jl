31c8560053d10c6fa4ce567d194764bd350c0e37
Felix Cremer <fcremer@bgc-jena.mpg.de>
2023-01-25 10:57:32 +0100

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.29

---
+++ src/MultipleTesting.jl
@@ -0,0 +1,5 @@
+module MultipleTesting
+
+# Write your package code here.
+
+end
---
+++ test/runtests.jl
@@ -0,0 +1,6 @@
+using MultipleTesting
+using Test
+
+@testset "MultipleTesting.jl" begin
+    # Write your tests here.
+end
